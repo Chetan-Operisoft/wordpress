38438f56481ab590f7e6df9e62940ddcf089af4c
Chetan-Operisoft <128120269+Chetan-Operisoft@users.noreply.github.com>
2023-04-04 16:10:31 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-set -e
 
-amazon-linux-extras install php7.4 -y
-yum install -y httpd mariadb-server php php-mysqlnd
-
-systemctl start httpd
-systemctl enable httpd
-systemctl start mariadb
-systemctl enable mariadb
+echo This is a simple string.
+echo "This is a string containing 'single quotes' so it's wrapped in double quotes."
+echo "This prints the user name:" $USER
+echo -e "The -e option lets us use\nformatting directives\nto split the string."
+sudo amazon-linux-extras install php7.4 -y
+sudo yum install -y httpd mariadb-server php php-mysqlnd
